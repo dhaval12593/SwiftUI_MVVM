6f06f64bdeab7920beea6e9fd4e2d07116373b38
Dhaval Adhav <dadhav@vrbo.com>
2022-08-02 09:24:14 -0500

    Initial Commit

---
+++ SwiftUIListMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIListMVVM
+//
+//  Created by Dhaval Sanjay Adhav on 8/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIListMVVM/SwiftUIListMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListMVVMApp.swift
+//  SwiftUIListMVVM
+//
+//  Created by Dhaval Sanjay Adhav on 8/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
